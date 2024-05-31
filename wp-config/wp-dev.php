e67b10791b794c36ba183d3a1da3c0bc146de80c
huy.dd <137746278+dinhhuy260390@users.noreply.github.com>
2024-05-31 17:20:33 +0700

    update dev DB

--- wp-config/wp-dev.php
+++ wp-config/wp-dev.php
@@ -10,10 +10,10 @@ define( 'DB_USER', 'root' );
 define( 'DB_PASSWORD', 'root' );
 
 /** Database hostname */
-define( 'DB_HOST', '127.0.0.1:3306' );
+define( 'DB_HOST', 'localhost' );
 
 /** Database charset to use in creating database tables. */
-define( 'DB_CHARSET', 'utf8mb4' );
+define( 'DB_CHARSET', 'utf8' );
 
 /** The database collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
