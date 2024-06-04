7051442b16d5be708d72b22ddf65f9a9e167c85a
huy.dd <137746278+dinhhuy260390@users.noreply.github.com>
2024-06-04 23:54:33 +0700

    update service

--- wp-content/themes/propell/single-service.php
+++ wp-content/themes/propell/single-service.php
@@ -205,67 +205,6 @@ $page = get_query_var('paged') ? get_query_var('paged') : 1;
         <?php endif; ?>
     </main>
 <?php endif; ?>
-
-<footer id="footer" class="footer mb-lg">
-    <div class="container">
-        <div class="footer__row01">
-            <div class="logo"><img src="<?php echo $assets ?>/img/logo-nav.png" alt="Logo"></div>
-            <div class="list">
-                <ul>
-                    <li>Electrical Engineering</li>
-                    <li>Extra Low Voltage System</li>
-                    <li>ACMV System</li>
-                    <li>Fire Prevention & Protection System</li>
-                    <li>ECO System</li>
-                    <li>Integrated Facilities Management</li>
-                </ul>
-            </div>
-            <div class="group">
-                <h2 class="c-title">There's Nothing <br/>We Can't Achieve Together</h2>
-                <div class="subcrice">
-                    <input class="subcrice__input" type="text" name="mail" placeholder="Your email...">
-                    <a href="#" class="subcrice__btn c-learn-more">LEARN MORE</a>
-                </div>
-                <ul class="partner only-pc">
-                    <li><img src="<?php echo $assets ?>/img/footer-logo01.png" alt=""></li>
-                    <li><img src="<?php echo $assets ?>/img/footer-logo02.png" alt=""></li>
-                    <li><img src="<?php echo $assets ?>/img/footer-logo03.png" alt=""></li>
-                    <li><img src="<?php echo $assets ?>/img/footer-logo04.png" alt=""></li>
-                    <li><img src="<?php echo $assets ?>/img/footer-logo05.png" alt=""></li>
-                    <li><img src="<?php echo $assets ?>/img/footer-logo06.png" alt=""></li>
-                </ul>
-                <div class="partner only-sp"><img src="<?php echo $assets ?>/img/footer-logo_sp.png" alt=""></div>
-            </div>
-        </div>
-        <div class="footer__row02">
-            <ul class="nav">
-                <li><a href="/contact-us/">Contact Us</a></li>
-                <li><a href="#">Propell Responsibilites</a></li>
-                <li><a href="/careers/">Careers</a></li>
-                <li><a href="/terms-of-use/">Terms of Use</a></li>
-                <li><a href="/policy/">Personal Data Protection Policy</a></li>
-            </ul>
-            <ul class="social">
-                <li><a href="#" target="_blank"><img src="<?php echo $assets ?>/img/icon-youtube.png" alt="youtube"></a></li>
-                <li><a href="#" target="_blank"><img src="<?php echo $assets ?>/img/icon-facebook.png" alt="facebook"></a></li>
-                <li><a href="#" target="_blank"><img src="<?php echo $assets ?>/img/icon-linkedin.png" alt="linkedin"></a></li>
-            </ul>
-            <p class="copyright">© 2023 Propell Integrated Pte Ltd .<br/>All rights reserved.</p>
-        </div>
-    </div>
-</footer>
-<!---->
-
-<!--<script src="--><?php //echo $assets ?><!--/libs/modernizr.min.js"></script>-->
-<!--<script src="--><?php //echo $assets ?><!--/libs/jquery-3.6.0.js"></script>-->
-<!--<script src="--><?php //echo $assets ?><!--/libs/slick.min.js"></script>-->
-<!--<script src="--><?php //echo $assets ?><!--/js/common.js"></script>-->
-<!---->
-
-<!--<script src="--><?php //echo $assets ?><!--/js/what-we-do.js"></script>-->
-<!--</body>-->
-<!---->
-<!--</html>-->
 <?php
 get_sidebar();
 get_footer();
