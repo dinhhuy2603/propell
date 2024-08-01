f6257aee73e03b4f77eeb7a524dd6a0e34b7705b
huy.doan <doandinhhuy2603@gmail.com>
2024-08-01 17:26:11 +0700

    add new config stg

--- wp-config/wp-dev.php
+++ wp-config/wp-dev.php
@@ -1,16 +1,16 @@
 <?php
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'propell' );
+define( 'DB_NAME', 'bitnami_wordpress' );
 
 /** Database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'bn_wordpress' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'root' );
+define( 'DB_PASSWORD', '70e77571f988be75b2e34c7298ffe1bac028c8669ad65be43fb5b234253edc27' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', '127.0.0.1:3306' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
